094b74706f2a2acc0436b54079861eda2f3ec861
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-05-14 06:31:26 +0530

    Add => for explanations

--- code/fp3_introduction/qsort.hs
+++ code/fp3_introduction/qsort.hs
@@ -1,14 +1,14 @@
--- [] empty list
--- [2] single ton list
--- [1, 2] ++ [3, 4, 5] list concatenation [1, 2, 3, 4, 5]
--- 1 : [2, 3, 4] adds to list [1, 2, 3, 4]
+-- [] -- => empty list
+-- [2] -- => single ton list
+-- [1, 2] ++ [3, 4, 5] -- => list concatenation [1, 2, 3, 4, 5]
+-- 1 : [2, 3, 4] -- => adds to list [1, 2, 3, 4]
 
--- [ a | a <- xs, a <= x] this is a list of elements such that for each 'a' belonging to 'xs' and a <= x
+-- [ a | a <- xs, a <= x] -- => this is a list of elements such that for each 'a' belonging to 'xs' and a <= x
 
 -- base case
 -- takes and returns an empty list
 qsort [] = []
--- [x: xs] is breaking up a list into x and [remaining elements]
+-- (x: xs) -- => is breaking up a list into x and [remaining elements]
 qsort (x : xs) = qsort ys ++ [x] ++ qsort zs
   where
     ys = [a | a <- xs, a <= x]
--- code/fp4_first_steps/code.hs
+++ code/fp4_first_steps/code.hs
@@ -1,22 +1,22 @@
 -- common list functions
--- head [1, 2, 3] is 1
--- tail [1, 2, 3] remove first element from list => [2, 3]
--- [1, 2, 3, 4] !! 2 gets element a index 2 => 3
--- take 3 [1, 2, 3, 4, 5] gets first 3 elements => [1, 2, 3]
--- drop 3 [1, 2, 3, 4, 5] removes first 3 elements => [4, 5]
--- length [1, 2, 3, 4, 5] => 5
--- sum [1, 2, 3, 4, 5] => 15
--- product [1, 2, 3, 4, 5] => 120
--- [1, 2, 3] ++ [3, 4] => [1, 2, 3, 3, 4]
--- reverse [1, 2, 3] => [3, 2, 1]
--- f a b => f(a, b)
--- f a + b => (f a) + b and not f(a + b) => functions have highest priority
+-- head [1, 2, 3] -- => 1
+-- tail [1, 2, 3] remove first element from list -- => [2, 3]
+-- [1, 2, 3, 4] !! 2 gets element a index 2 -- => 3
+-- take 3 [1, 2, 3, 4, 5] gets first 3 elements -- => [1, 2, 3]
+-- drop 3 [1, 2, 3, 4, 5] removes first 3 elements -- => [4, 5]
+-- length [1, 2, 3, 4, 5] -- => 5
+-- sum [1, 2, 3, 4, 5] -- => 15
+-- product [1, 2, 3, 4, 5] -- => 120
+-- [1, 2, 3] ++ [3, 4] -- => [1, 2, 3, 3, 4]
+-- reverse [1, 2, 3] -- => [3, 2, 1]
+-- f a b -- => f(a, b)
+-- f a + b -- => (f a) + b and not f(a + b) -- => functions have highest priority
 -- Math to haskell representation
--- f(x) => f x
--- f(x, y) => f x y
--- f(g(x)) => f (g x)
--- f(x, g(y)) => f x (g y)
--- f(x) * g(y) => f x * g y
+-- f(x) -- => f x
+-- f(x, y) -- => f x y
+-- f(g(x)) -- => f (g x)
+-- f(x, g(y)) -- => f x (g y)
+-- f(x) * g(y) -- => f x * g y
 -- New functions defined with script, a text file consisting of function definitions
 -- usually have .hs but not mandatory
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
@@ -37,11 +37,11 @@ average ns = sum ns `div` length ns
 -- :load <file>
 -- :reload
 -- :set editor <name>
--- :edit <name> => edit script name
--- :edit => edit current script
--- :type <expr> => show type of expression
--- :? => show all commands
--- :quit => quit ghci
+-- :edit <name> -- => edit script name
+-- :edit -- => edit current script
+-- :type <expr> -- => show type of expression
+-- :? -- => show all commands
+-- :quit -- => quit ghci
 
 -- mimic last function
 last1 ns = head (reverse ns)
