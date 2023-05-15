030b1f0061e56b658afa9d057d48f48828d3ca7e
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2023-05-15 09:29:22 +0530

    fp1 move notes to separate file

--- code/fp3_introduction/qsort.hs
+++ code/fp3_introduction/qsort.hs
@@ -1,10 +1,3 @@
--- [] -- => empty list
--- [2] -- => single ton list
--- [1, 2] ++ [3, 4, 5] -- => list concatenation [1, 2, 3, 4, 5]
--- 1 : [2, 3, 4] -- => adds to list [1, 2, 3, 4]
-
--- [ a | a <- xs, a <= x] -- => this is a list of elements such that for each 'a' belonging to 'xs' and a <= x
-
 -- base case
 -- takes and returns an empty list
 qsort [] = []
